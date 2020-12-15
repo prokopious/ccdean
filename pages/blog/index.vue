7495566e80799c48ad7abd1cbd0a4c6b102f0a92
tmhuyett <toddhuyett@gmail.com>
2020-12-14 19:46:13 -0500

    'd'

---
+++ pages/blog/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h2 class="subtitle">
+      We build affordable, low-profile fences to protect your landscaping from
+      whitetail deer and other nuisance species. Quick fixes simply don't work
+      against adaptable urban wildlife. Consider an old-fashioned fence and go
+      to bed knowing that your hard work is safe.
+    </h2>
+    <div class="phone">703-963-8546</div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 10;
+  background-color: #c9c8a5;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.phone {
+  font-size: 40px;
+}
+
+.title {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  padding-bottom: 30px;
+  font-weight: bold;
+  style: italic;
+  font-size: 2.7rem;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  padding: 30px;
+  font-size: 2rem;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+a {
+  font-weight: bold;
+  color: black;
+  font-size: 50;
+  style: none;
+}
+.deploy-button {
+  margin-top: 1rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <logo />
+    <h1 class="title">WHITETAIL BARRIERS OF LYNCHBURG</h1>
+    <h2 class="subtitle">Protect your Landscaping with Low-Profile Fencing</h2>
+   <div class="phone">703-963-8546</div>
+      <nuxt-link to="/blog">our services</nuxt-link>
+  
+     
+    
+   
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  background-color: #89b099;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.phone {
+font-size: 40px;
+}
+
+.title {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: bold;
+  font-size: 2.7rem;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 2rem;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 10px;
+}
+a {
+  font-weight: bold;
+  color: black;
+  font-size: 50;
+  style: none;
+}
+.deploy-button {
+  margin-top: 1rem;
+}
+</style>
